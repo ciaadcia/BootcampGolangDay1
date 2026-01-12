f583d277df6c71e0deda27e2a4151b8908cce736
ciaa <154084369+ciaadcia@users.noreply.github.com>
2026-01-12 15:32:32 +0700

    Update main.go

--- main.go
+++ main.go
@@ -6,40 +6,47 @@ import (
 )
 
 func main() {
-
-	fmt.Println("No 1 - Find Divisor")
+	fmt.Println("No 1")
 	findDivisor(6)
 	findDivisor(24)
 	findDivisor(7)
 
-	fmt.Println("\nNo 2 - Extract Digit")
+	fmt.Println("No 2")
 	extractDigit(12234)
 	extractDigit(5432)
 	extractDigit(1278)
 
-	fmt.Println("\nNo 3 - Triangle Star")
-	triangleStar(5)
+	fmt.Println("No 3")
+	triangleStarLeft(5)
+	fmt.Println()
+	triangleStarRight(5)
 
-	fmt.Println("\nNo 4 - Number Pyramid")
+	fmt.Println("No 4")
 	numberPyramidInput()
 
-	fmt.Println("\nNo 8 - Palindrome String")
+	fmt.Println("No 5")
+	patternFive(5)
+
+	fmt.Println("No 6")
+	patternSix(5)
+
+	fmt.Println("No 8")
 	fmt.Println(isPalindrome("Kasur ini rusak"))
 	fmt.Println(isPalindrome("tamaT"))
 	fmt.Println(isPalindrome("Aku Usa"))
 
-	fmt.Println("\nNo 9 - Reverse String")
+	fmt.Println("No 9")
 	fmt.Println(reverse("ABCD"))
 	fmt.Println(reverse("tamaT"))
 	fmt.Println(reverse("XYnb"))
 
-	fmt.Println("\nNo 10 - Check Braces")
+	fmt.Println("No 10")
 	fmt.Println(checkBraces("()"))
 	fmt.Println(checkBraces("()()"))
 	fmt.Println(checkBraces("(()"))
 	fmt.Println(checkBraces("()(()"))
 
-	fmt.Println("\nNo 11 - Palindrome Number")
+	fmt.Println("No 11")
 	fmt.Println(isNumberPalindrome(121))
 	fmt.Println(isNumberPalindrome(2147447412))
 	fmt.Println(isNumberPalindrome(333))
@@ -47,9 +54,6 @@ func main() {
 	fmt.Println(isNumberPalindrome(11))
 }
 
-/*
-No 1
-*/
 func findDivisor(n int) {
 	for i := 1; i <= n; i++ {
 		if n%i == 0 {
@@ -59,9 +63,6 @@ func findDivisor(n int) {
 	fmt.Println()
 }
 
-/*
-No 2
-*/
 func extractDigit(n int) {
 	for n > 0 {
 		fmt.Print(n%10, " ")
@@ -70,60 +71,75 @@ func extractDigit(n int) {
 	fmt.Println()
 }
 
-/*
-No 3
-*/
-func triangleStar(n int) {
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			if j >= i {
-				fmt.Print("* ")
-			} else {
-				fmt.Print("  ")
-			}
+func triangleStarLeft(n int) {
+	for i := n; i >= 1; i-- {
+		for j := 1; j <= i; j++ {
+			fmt.Print("* ")
 		}
 		fmt.Println()
 	}
 }
 
-/*
-No 4
-*/
+func triangleStarRight(n int) {
+	for i := 1; i <= n; i++ {
+		for s := 1; s <= n-i; s++ {
+			fmt.Print("  ")
+		}
+		for j := 1; j <= i; j++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
+}
+
 func numberPyramidInput() {
 	var n int
 	fmt.Print("Masukkan jumlah baris piramida: ")
 	fmt.Scanln(&n)
 
 	for i := n; i >= 1; i-- {
-
-		for s := 0; s < n-i; s++ {
-			fmt.Print("  ")
-		}
-
 		for j := i; j >= 1; j-- {
 			fmt.Print(j, " ")
 		}
-
 		for j := 2; j <= i; j++ {
 			fmt.Print(j, " ")
 		}
-
 		fmt.Println()
 	}
 }
 
-/*
-No 8
-*/
+func patternFive(n int) {
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			if j%2 == 1 {
+				fmt.Print(i, " ")
+			} else {
+				fmt.Print(n-i+1, " ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func patternSix(n int) {
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			if (i+j)%2 == 0 {
+				fmt.Print("- ")
+			} else {
+				fmt.Print(j, " ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
 func isPalindrome(s string) bool {
 	s = strings.ToLower(s)
 	s = strings.ReplaceAll(s, " ", "")
 	return s == reverse(s)
 }
 
-/*
-No 9
-*/
 func reverse(s string) string {
 	r := ""
 	for i := len(s) - 1; i >= 0; i-- {
@@ -132,9 +148,6 @@ func reverse(s string) string {
 	return r
 }
 
-/*
-No 10
-*/
 func checkBraces(s string) bool {
 	count := 0
 	for _, c := range s {
@@ -150,13 +163,9 @@ func checkBraces(s string) bool {
 	return count == 0
 }
 
-/*
-No 11
-*/
 func isNumberPalindrome(n int) bool {
 	ori := n
 	rev := 0
-
 	for n > 0 {
 		rev = rev*10 + n%10
 		n /= 10
